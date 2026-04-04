a26c3bfa63bcd00318d58ddbefc1cc1a66aad982
gyouz <gyouz@192.168.0.8>
2026-04-05 01:37:19 +0900

    一時的な変更

--- ga4/daily_screenPageViews_log.py
+++ ga4/daily_screenPageViews_log.py
@@ -13,11 +13,9 @@ def update_daily_screenPageViews_log(client):
     os.makedirs("ga4Data", exist_ok=True)
     daily_file = "ga4Data/daily_screenPageViews.csv"
 
-    today = (datetime.now().date() - timedelta(days=5)).strftime("%Y-%m-%d")
-    yesterday = (datetime.now().date() - timedelta(days=4)).strftime("%Y-%m-%d")
+    today = (datetime.now().date() - timedelta(days=7)).strftime("%Y-%m-%d")
+    yesterday = (datetime.now().date() - timedelta(days=6)).strftime("%Y-%m-%d")
 
-    #a6days_ago = (datetime.now().date() - timedelta(days=6)).strftime("%Y-%m-%d")
-    #a7days_ago = (datetime.now().date() - timedelta(days=7)).strftime("%Y-%m-%d")
     #a8days_ago = (datetime.now().date() - timedelta(days=8)).strftime("%Y-%m-%d")
     #a9days_ago = (datetime.now().date() - timedelta(days=9)).strftime("%Y-%m-%d")
 
