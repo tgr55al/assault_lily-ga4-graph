f984758151810b90660cd7d9ea72b24c9464aa7e
gyouz <gyouz@192.168.0.8>
2026-04-05 01:33:38 +0900

    一時的な変更

--- ga4/daily_screenPageViews_log.py
+++ ga4/daily_screenPageViews_log.py
@@ -2,6 +2,7 @@ import csv
 import os
 from google.analytics.data_v1beta.types import RunReportRequest, DateRange, Dimension, Metric
 from ga4.date_utils import get_today, get_yesterday
+from datetime import datetime, timedelta
 
 PROPERTY_ID = "530080930"
 
@@ -12,8 +13,17 @@ def update_daily_screenPageViews_log(client):
     os.makedirs("ga4Data", exist_ok=True)
     daily_file = "ga4Data/daily_screenPageViews.csv"
 
-    today = get_today()
-    yesterday = get_yesterday()
+    today = (datetime.now().date() - timedelta(days=3)).strftime("%Y-%m-%d")
+    yesterday = (datetime.now().date() - timedelta(days=2)).strftime("%Y-%m-%d")
+
+    #a2days_ago = (datetime.now().date() - timedelta(days=2)).strftime("%Y-%m-%d")
+    #a3days_ago = (datetime.now().date() - timedelta(days=3)).strftime("%Y-%m-%d")
+    #a4days_ago = (datetime.now().date() - timedelta(days=4)).strftime("%Y-%m-%d")
+    #a5days_ago = (datetime.now().date() - timedelta(days=5)).strftime("%Y-%m-%d")
+    #a6days_ago = (datetime.now().date() - timedelta(days=6)).strftime("%Y-%m-%d")
+    #a7days_ago = (datetime.now().date() - timedelta(days=7)).strftime("%Y-%m-%d")
+    #a8days_ago = (datetime.now().date() - timedelta(days=8)).strftime("%Y-%m-%d")
+    #a9days_ago = (datetime.now().date() - timedelta(days=9)).strftime("%Y-%m-%d")
 
     # 今日
     request_today = RunReportRequest(
